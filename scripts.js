2e72816ad3cf3034b6c4b398dc4fde07dabaf23f
nikolinef <126453544+nikolinef@users.noreply.github.com>
2024-02-27 10:22:58 +0100

    update html og kommentar

--- scripts.js
+++ scripts.js
@@ -1,13 +1,16 @@
-const burgerMenu = document.getElementById("burgermenu");
-const header = document.querySelector("header");
+const burgerMenu = document.getElementById("burgermenu"); 
+const header = document.querySelector("header"); /*kan fange class elementer*/
 
-burgerMenu.addEventListener("click", () => {
-    burgerMenu.classList.toggle("active");
-    header.classList.toggle("active");
+burgerMenu.addEventListener("click", () => { /*funktion udløst af click */
+    burgerMenu.classList.toggle("active"); /*tilføjer en ny class på burgermenuen*/
+    header.classList.toggle("active"); /*tilføjer en ny class på headeren*/
 })
 
 
-document.querySelectorAll(".menua").forEach(n => n.addEventListener("click", () => {
-    burgerMenu.classList.remove("active");
+document.querySelectorAll(".menua").forEach(n => n.addEventListener("click", () => { 
+    /*Jeg har lavet en ny class, menu a, som js kan hente og lave den samme funktion for hvert link udløst af en eventlistener*/
+    burgerMenu.classList.remove("active"); 
         header.classList.remove("active");
     }))
+    /*funktionen fjerne active class, så  hver gang man trykker på et af linksne vil menuen lukke igen, 
+    fordi jeg har skrevet i css at headeren og burgermenu ikke skal vises, når .active class ikke er "aktivt"*/
