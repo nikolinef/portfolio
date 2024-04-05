79741d5bf3a783e5ff93c6c31c8e5ccf14ec6477
nikolinef <126453544+nikolinef@users.noreply.github.com>
2024-04-06 00:10:23 +0200

    update html, css og js

--- scripts.js
+++ scripts.js
@@ -14,3 +14,32 @@ document.querySelectorAll(".menua").forEach(n => n.addEventListener("click", () 
     }))
     /*funktionen fjerne active class, så  hver gang man trykker på et af linksne vil menuen lukke igen, 
     fordi jeg har skrevet i css at headeren og burgermenu ikke skal vises, når .active class ikke er "aktivt"*/
+
+//scripts til carousel
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("carouselcard");
+    let dots = document.getElementsByClassName("dot");
+    
+    if(n > slides.length) {slideIndex = 1}
+    if(n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace("activeSlide", "");
+    }
+    slides[slideIndex-1].style.display = "flex";
+    dots[slideIndex-1].className += " activeSlide";
+}
